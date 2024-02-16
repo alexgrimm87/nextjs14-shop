41316a46662acfa3ff5e95bd5ab69e0d13e89278
Oleksandr Grymashevych <alexgrimm87@gmail.com>
2024-02-16 21:13:57 +0200

    add product card

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
-import Link from "next/link";
 import Head from 'next/head';
 import {getProducts} from '../lib/products';
 import Title from '../components/Title';
+import ProductCard from "../components/ProductCard";
 
 export async function getStaticProps() {
   const products = await getProducts();
@@ -20,12 +20,10 @@ function HomePage({ products }) {
       </Head>
       <main className="px-6 py-4">
         <Title>Next Shop</Title>
-        <ul>
+        <ul className="grid grid-cols-1 lg:grid-cols-3 gap-4">
           {products.map((product) => (
             <li key={product.id}>
-              <Link href={`/products/${product.id}`}>
-                {product.title}
-              </Link>
+              <ProductCard product={product} />
             </li>
           ))}
         </ul>
